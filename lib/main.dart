dcc9bfc1d65c18223baf7312dc5863e2ac68c25d
rapaka-usha <147684261+rapaka-usha@users.noreply.github.com>
2024-11-11 10:37:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lab9/login.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyD_1fqXPeZtdooIJJH9B0y-_-ZZQPYNBl4",
+        appId: "1:1046931687897:android:2ad7be32ad5d6e3b9031c8",
+        messagingSenderId: "1046931687897",
+        projectId: "lab9-2d0bc"),
+  );
   runApp(MyApp());
 }
 
